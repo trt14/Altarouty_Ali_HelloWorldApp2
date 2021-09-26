ec170c1f4aa52c231bcb3d731a4c3484d16eef8d
trt14 <tarooti14dev@gmail.com>
2021-09-26 13:05:02 +0300

    add

---
+++ HelloWorld3 2/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        var num1:Int=2
+//        var num2:Int=3
+//
+//        var sum:Int=calc(num1,num2)
+//        if(sum<=10){
+//            Log.d("Calc", "This is Sum of $num1+$num2=$sum ,less than 10")
+//
+//        }else{
+//            Log.d("Calc", "This is Sum of $num1+$num2=$sum ,greater than 10")
+//        }
+//
+//    }
+//
+//    fun calc(num1:Int,num2:Int):Int{
+//        var sum:Int=num1+num2
+//        return sum
+//    }
+    }
+}
